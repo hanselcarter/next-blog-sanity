f60430a229a224718f060886f183ac9ff139ff87
hanselcarter <barahonahansel@hotmail.com>
2024-10-19 17:09:17 -0600

    chore: nav link to

--- app/components/nav/menuItem.tsx
+++ app/components/nav/menuItem.tsx
@@ -1,15 +1,21 @@
+import Link from "next/link";
+
 interface MenuItemProps {
   title: string;
+  linkTo: string;
 }
-function MenuItem({ title }: MenuItemProps) {
+function MenuItem({ title, linkTo }: MenuItemProps) {
   return (
-    <div className=" flex-col gap-[40px] hidden xs-nav-menu:flex">
+    <Link
+      href={linkTo}
+      className=" flex-col gap-[40px] hidden xs-nav-menu:flex"
+    >
       <p className="text-black text-center font-['New_York_Small'] text-[20px] font-normal leading-[170%] tracking-[2.4px] uppercase">
         {title}
       </p>
 
       <div className="w-[66px] h-[2px] bg-black"></div>
-    </div>
+    </Link>
   );
 }
 
--- app/components/nav/navbar.tsx
+++ app/components/nav/navbar.tsx
@@ -7,7 +7,7 @@ function NavBar() {
     <nav className="w-full shadow-[0px_1px_0px_0px_rgba(0,0,0,0.16)] flex justify-between pt-[52.4px] px-[63.58px] items-baseline">
       <Image src={Logo} width={396} height={37} alt="logo" priority />
 
-      <MenuItem title="Blog" />
+      <MenuItem title="Blog" linkTo="/" />
     </nav>
   );
 }
